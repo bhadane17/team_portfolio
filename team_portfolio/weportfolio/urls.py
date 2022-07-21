2ce43549c2d66d82d8936a815407336242d2abbf
bhadane17 <95074297+bhadane17@users.noreply.github.com>
2022-07-21 19:32:08 +0530

    files upload

---
+++ team_portfolio/weportfolio/urls.py
@@ -0,0 +1,7 @@
+# from django.urls import path
+# from weportfolio import views
+#
+# urlpatterns = [
+#     path('', views.home, name='blog-home'),
+#     path('', views.index, name='index.html'),
+#]
